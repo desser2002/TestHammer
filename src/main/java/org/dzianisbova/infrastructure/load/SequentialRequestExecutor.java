662b64370e39ed3a7ea451bf180f29364dac9073
bovadenis <bovadzianis@gmail.con>
2025-10-19 19:45:50 +0200

    Implement HttpRequestComposer for building HTTP requests and refactor SequentialRequestExecutor to use it

--- src/main/java/org/dzianisbova/infrastructure/load/SequentialRequestExecutor.java
+++ src/main/java/org/dzianisbova/infrastructure/load/SequentialRequestExecutor.java
@@ -1,17 +1,15 @@
 package org.dzianisbova.infrastructure.load;
 
 import org.dzianisbova.domain.api.Request;
+import org.dzianisbova.domain.load.HttpRequestComposer;
 import org.dzianisbova.domain.load.RequestExecutor;
 import org.dzianisbova.domain.logging.Logger;
 import org.dzianisbova.domain.metrics.StatisticsService;
 import org.dzianisbova.domain.response.Response;
 
-import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
 
@@ -29,8 +27,7 @@ public class SequentialRequestExecutor implements RequestExecutor {
 
     @Override
     public Response execute(Request request) {
-        String fullUrl = buildFullUrl(request);
-        HttpRequest httpRequest = buildHttpRequest(request, fullUrl);
+        HttpRequest httpRequest = HttpRequestComposer.composeHttpRequest(request);
         Instant startTime = Instant.now();
         try {
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
@@ -51,31 +48,5 @@ public class SequentialRequestExecutor implements RequestExecutor {
 
             return new Response(-1, e.getMessage(), duration);
         }
-    }
-
-    private String buildFullUrl(Request request) {
-        StringBuilder url = new StringBuilder(request.getUrl());
-        var queryParams = request.getQueryParams();
-        if (!queryParams.isEmpty()) {
-            url.append("?");
-            queryParams.forEach((key, value) -> {
-                url.append(URLEncoder.encode(key, StandardCharsets.UTF_8));
-                url.append("=");
-                url.append(URLEncoder.encode(value, StandardCharsets.UTF_8));
-                url.append("&");
-            });
-            url.deleteCharAt(url.length() - 1);
-        }
-        return url.toString();
-    }
-
-    private HttpRequest buildHttpRequest(Request request, String url) {
-        HttpRequest.Builder builder = HttpRequest.newBuilder().uri(URI.create(url));
-        HttpRequest.BodyPublisher bodyPublisher = (request.hasBody())
-                ? HttpRequest.BodyPublishers.ofString(request.getRequestBody())
-                : HttpRequest.BodyPublishers.noBody();
-        builder.method(request.getMethod().name(), bodyPublisher);
-        request.getHeaders().forEach(builder::header);
-        return builder.build();
     }
 }
